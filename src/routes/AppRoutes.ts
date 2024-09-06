eeb599284bf684a50fa2a003936ce3c4e8193d78
macluiggy <luiggy.macias@imagineapps.co>
2024-09-06 13:51:20 -0500

    feat: Remove unused routes from AppRoutes

--- src/routes/AppRoutes.ts
+++ src/routes/AppRoutes.ts
@@ -1,9 +1,3 @@
-/**{
-    "fileName": "c120348d-510a-4fad-b056-6a377af961db.pdf",
-    "url": "https://surone-formvinculacion-dev.s3.us-west-2.amazonaws.com/dev/personas-naturales/20240906092745.pdf",
-    "s3Key": "dev/personas-naturales/20240906092745.pdf"
-} */
-
 import { Router } from "express";
 
 const appRoutes = Router();
